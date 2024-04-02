dbf2f4843136843d72b0dd527a814d2e10523f07
LazoJankovic <lazar.jankovic@codaxy.com>
2024-04-02 17:16:50 +0200

    add rewriter and create custom middleware

--- dotnet_practice/Program.cs
+++ dotnet_practice/Program.cs
@@ -1,9 +1,19 @@
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Rewrite;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 app.Urls.Add("http://localhost:5144");
+
+app.UseRewriter(new RewriteOptions().AddRedirect("tasks/(.*)", "todos/$1"));
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow}] Started.");
+    await next(context); //?
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path}  {DateTime.UtcNow}] Finished.");
+});
+
 
 // app.MapGet("/", () => "Hello World!");
 var todos = new List<Todo>();
@@ -20,7 +30,7 @@ app.MapPost("/todos", (Todo task) => {
     return TypedResults.Created("/todos{id}", task);
 });
 
-app.MapDelete("/todos/{id}", (int id) =>
+app.MapDelete("/todos/{id}", (int id) =>   
 {
     todos.RemoveAll(t => t.Id == id);
     return TypedResults.NoContent();
