d173dcae6576fa073c246e9fee335bc70264ebc8
LazoJankovic <lazar.jankovic@codaxy.com>
2024-04-02 16:34:27 +0200

    create todos endpoints and http requests

--- dotnet_practice/Program.cs
+++ dotnet_practice/Program.cs
@@ -1,6 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Http.HttpResults;
 
-//app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+app.Urls.Add("http://localhost:5144");
+
+// app.MapGet("/", () => "Hello World!");
+var todos = new List<Todo>();
+
+app.MapGet("/todos", () => todos); //minimal APIs understands  w/o strongly typed results
+
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) => {
+    var targetTodo = todos.SingleOrDefault(t => id == t.Id);
+    return targetTodo is null ? TypedResults.NotFound() : TypedResults.Ok(targetTodo);
+});
+
+app.MapPost("/todos", (Todo task) => {
+    todos.Add(task);
+    return TypedResults.Created("/todos{id}", task);
+});
+
+app.MapDelete("/todos/{id}", (int id) =>
+{
+    todos.RemoveAll(t => t.Id == id);
+    return TypedResults.NoContent();
+});
 
 app.Run();
+
+
+public record Todo(int Id, string Name, DateTime DueData, bool isCompleted);
